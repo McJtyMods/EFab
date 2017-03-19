b37d9c33d08dc2d535dac8852c48ca7cd54bdfee
McJty <mcjty1@gmail.com>
2017-03-19 06:54:18 +0100

    More model work

--- src/main/java/mcjty/efab/blocks/ModBlocks.java
+++ src/main/java/mcjty/efab/blocks/ModBlocks.java
@@ -1,6 +1,7 @@
 package mcjty.efab.blocks;
 
 import mcjty.efab.blocks.base.BaseBlock;
+import mcjty.efab.blocks.gearbox.GearBoxBlock;
 import mcjty.efab.blocks.grid.GridBlock;
 import mcjty.efab.blocks.pipes.PipeBlock;
 import net.minecraftforge.fml.relauncher.Side;
@@ -9,11 +10,13 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class ModBlocks {
 
     public static BaseBlock baseBlock;
+    public static GearBoxBlock gearBoxBlock;
     public static PipeBlock pipeBlock;
     public static GridBlock gridBlock;
 
     public static void init() {
         baseBlock = new BaseBlock();
+        gearBoxBlock = new GearBoxBlock();
         pipeBlock = new PipeBlock();
         gridBlock = new GridBlock();
     }
@@ -21,6 +24,7 @@ public class ModBlocks {
     @SideOnly(Side.CLIENT)
     public static void initModels() {
         baseBlock.initModel();
+        gearBoxBlock.initModel();
         pipeBlock.initModel();
         gridBlock.initModel();
     }
---
+++ src/main/java/mcjty/efab/blocks/gearbox/GearBoxBlock.java
@@ -0,0 +1,11 @@
+package mcjty.efab.blocks.gearbox;
+
+import mcjty.efab.blocks.GenericEFabBlock;
+import net.minecraft.block.material.Material;
+
+public class GearBoxBlock extends GenericEFabBlock {
+
+    public GearBoxBlock() {
+        super(Material.IRON, "gearbox");
+    }
+}
