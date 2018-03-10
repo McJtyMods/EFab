ff4a04a2df3a10aab570403cef902bef353e0762
McJty <mcjty1@gmail.com>
2018-03-10 06:22:00 +0100

    TOP support for the grid and crafters

--- src/main/java/mcjty/efab/blocks/crafter/CrafterBlock.java
+++ src/main/java/mcjty/efab/blocks/crafter/CrafterBlock.java
@@ -4,10 +4,13 @@ import mcjty.efab.blocks.GenericEFabMultiBlockPart;
 import mcjty.efab.proxy.GuiProxy;
 import mcjty.lib.container.BaseBlock;
 import mcjty.lib.container.GenericGuiContainer;
+import mcjty.theoneprobe.api.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
@@ -15,6 +18,7 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.Optional;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -45,6 +49,31 @@ public class CrafterBlock extends GenericEFabMultiBlockPart<CrafterTE, CrafterCo
         tooltip.add(TextFormatting.WHITE + "Also needs a " + TextFormatting.GREEN + "processor" + TextFormatting.WHITE
                 + " and one or more " + TextFormatting.GREEN + "storage" + TextFormatting.WHITE + " blocks");
         tooltip.add(TextFormatting.YELLOW + "The crafter needs a redstone signal to work!");
+    }
+
+    @Override
+    @Optional.Method(modid = "theoneprobe")
+    public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, IBlockState blockState, IProbeHitData data) {
+        super.addProbeInfo(mode, probeInfo, player, world, blockState, data);
+        TileEntity te = world.getTileEntity(data.getPos());
+        if (te instanceof CrafterTE) {
+            CrafterTE crafterTE = (CrafterTE) te;
+            if (!crafterTE.isOn()) {
+                probeInfo.text(TextStyleClass.LABEL + "Status " + TextStyleClass.INFO + "OFF");
+            } else if (crafterTE.isCrafting()) {
+                probeInfo.text(TextStyleClass.LABEL + "Status " + TextStyleClass.INFO + crafterTE.getProgress() + "%");
+            } else if (crafterTE.getLastError() == null || crafterTE.getLastError().trim().isEmpty()) {
+                probeInfo.text(TextStyleClass.LABEL + "Status " + TextStyleClass.INFO + "IDLE");
+            } else {
+                probeInfo.text(TextStyleClass.LABEL + "Status " + TextStyleClass.ERROR + crafterTE.getLastError());
+            }
+            List<ItemStack> outputs = crafterTE.getOutputs();
+            if (!outputs.isEmpty()) {
+                probeInfo.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER))
+                    .text(TextStyleClass.LABEL + "Output ")
+                    .item(outputs.get(0));
+            }
+        }
     }
 
     @SideOnly(Side.CLIENT)
--- src/main/java/mcjty/efab/blocks/grid/GridBlock.java
+++ src/main/java/mcjty/efab/blocks/grid/GridBlock.java
@@ -2,23 +2,28 @@ package mcjty.efab.blocks.grid;
 
 import mcjty.efab.blocks.GenericEFabBlockWithTE;
 import mcjty.efab.blocks.ModBlocks;
+import mcjty.efab.blocks.crafter.CrafterTE;
 import mcjty.efab.proxy.GuiProxy;
 import mcjty.efab.sound.ISoundProducer;
 import mcjty.lib.container.BaseBlock;
 import mcjty.lib.container.GenericGuiContainer;
+import mcjty.theoneprobe.api.*;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.Optional;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -84,6 +89,40 @@ public class GridBlock extends GenericEFabBlockWithTE<GridTE, GridContainer> imp
         // @todo: on 1.11 we could have used the position from which the update is coming
         world.markBlockRangeForRenderUpdate(pos, pos);
     }
+
+    @Override
+    @Optional.Method(modid = "theoneprobe")
+    public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, IBlockState blockState, IProbeHitData data) {
+        super.addProbeInfo(mode, probeInfo, player, world, blockState, data);
+        TileEntity te = world.getTileEntity(data.getPos());
+        if (te instanceof GridTE) {
+            GridTE gridTE = (GridTE) te;
+            List<String> errorState = gridTE.getErrorState();
+            if (!errorState.isEmpty()) {
+                for (String error : errorState) {
+                    probeInfo.text(TextStyleClass.LABEL + "Status " + TextStyleClass.ERROR + error);
+                }
+            } else {
+                int ticks = gridTE.getTicksRemaining();
+                if (ticks < 0) {
+                    probeInfo.text(TextStyleClass.LABEL + "Status " + TextStyleClass.INFO + "IDLE");
+                } else {
+                    int total = gridTE.getTotalTicks();
+                    if (total > 0) {
+                        int progress = (total-ticks) * 100 / total;
+                        probeInfo.text(TextStyleClass.LABEL + "Status " + TextStyleClass.INFO + progress + "%");
+                    }
+                }
+            }
+            List<ItemStack> outputs = gridTE.getOutputs();
+            if (!outputs.isEmpty()) {
+                probeInfo.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER))
+                        .text(TextStyleClass.LABEL + "Output ")
+                        .item(outputs.get(0));
+            }
+        }
+    }
+
 
 //    @Override
 //    public EnumBlockRenderType getRenderType(IBlockState state) {
