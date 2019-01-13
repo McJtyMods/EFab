b22f7887172682f5e03bf8da5a8ff59d18ffb9a1
McJty <mcjty1@gmail.com>
2019-01-13 09:25:30 +0100

    Version 0.1.2

--- src/main/java/mcjty/efab/EFab.java
+++ src/main/java/mcjty/efab/EFab.java
@@ -26,8 +26,8 @@ public class EFab implements ModBase {
 
     public static final String MODID = "efab";
     public static final String MODNAME = "EFab";
-    public static final String MODVERSION = "0.1.1";
-    public static final String MIN_MCJTYLIB_VER = "3.0.0";
+    public static final String MODVERSION = "0.1.2";
+    public static final String MIN_MCJTYLIB_VER = "3.1.0";
 
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
 
