629c6e9aafe9b6f0fe4e7ad55f79b0dc65d03a72
McJty <mcjty1@gmail.com>
2019-03-07 20:25:38 +0100

    Big refactor of the networking system. Added some 1.13 look-a-like api for it

--- src/main/java/mcjty/efab/network/PacketGetMonitorText.java
+++ src/main/java/mcjty/efab/network/PacketGetMonitorText.java
@@ -1,37 +1,65 @@
 package mcjty.efab.network;
 
-import mcjty.efab.EFab;
-import mcjty.lib.network.PacketRequestServerList;
-import mcjty.lib.network.PacketRequestServerListHandler;
+import io.netty.buffer.ByteBuf;
+import mcjty.lib.network.ICommandHandler;
+import mcjty.lib.network.NetworkTools;
+import mcjty.lib.network.TypedMapTools;
 import mcjty.lib.typed.Type;
 import mcjty.lib.typed.TypedMap;
+import mcjty.lib.varia.Logging;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
-import javax.annotation.Nonnull;
 import java.util.List;
 
-public class PacketGetMonitorText extends PacketRequestServerList<String> {
+public class PacketGetMonitorText implements IMessage {
 
     public static String CMD_GETMESSAGES = "getMessages";
     public static String CLIENTCMD_GETMESSAGES = "getMessages";
+    protected BlockPos pos;
+    protected TypedMap params;
 
     public PacketGetMonitorText() {
     }
 
     public PacketGetMonitorText(BlockPos pos) {
-        super(EFab.MODID, pos, CMD_GETMESSAGES, TypedMap.EMPTY);
+        this.pos = pos;
+        this.params = TypedMap.EMPTY;
     }
 
-    public static class Handler extends PacketRequestServerListHandler<PacketGetMonitorText, String> {
+    @Override
+    public void fromBytes(ByteBuf buf) {
+        pos = NetworkTools.readPos(buf);
+        params = TypedMapTools.readArguments(buf);
+    }
 
-        public Handler() {
-            super(Type.STRING);
-        }
+    @Override
+    public void toBytes(ByteBuf buf) {
+        NetworkTools.writePos(buf, pos);
+        TypedMapTools.writeArguments(buf, params);
+    }
+
+    public static class Handler implements IMessageHandler<PacketGetMonitorText, IMessage> {
 
         @Override
-        protected void sendToClient(BlockPos pos, @Nonnull List<String> list, MessageContext messageContext) {
-            EFabMessages.INSTANCE.sendTo(new PacketMonitorTextReady(pos, CLIENTCMD_GETMESSAGES, list), messageContext.getServerHandler().player);
+        public IMessage onMessage(mcjty.efab.network.PacketGetMonitorText message, MessageContext ctx) {
+            FMLCommonHandler.instance().getWorldThread(ctx.netHandler).addScheduledTask(() -> handle(message, ctx));
+            return null;
+        }
+
+        private void handle(mcjty.efab.network.PacketGetMonitorText message, MessageContext ctx) {
+            TileEntity te = ctx.getServerHandler().player.getEntityWorld().getTileEntity(message.pos);
+            if(!(te instanceof ICommandHandler)) {
+                Logging.log("TileEntity is not a CommandHandler!");
+                return;
+            }
+            ICommandHandler commandHandler = (ICommandHandler) te;
+            List<String> list = commandHandler.executeWithResultList(CMD_GETMESSAGES, message.params, Type.STRING);
+            EFabMessages.INSTANCE.sendTo(new PacketMonitorTextReady(message.pos, CLIENTCMD_GETMESSAGES, list), ctx.getServerHandler().player);
         }
     }
 }
